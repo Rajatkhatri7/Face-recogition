c07be5fb9b2c1c4d23f5b47c472b557202627c54
Rajat Khatri <rajatkhatri0002@gmail.com>
2020-05-15 19:16:30 +0530

    added  eyes capturing

--- facepic.py
+++ facepic.py
@@ -6,8 +6,10 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
+
 img = cv2.imread('IMG_20191109_181046.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
 faces = face_cascade.detectMultiScale(gray,1.1,4)
 
 for (x,y,w,h) in faces:
--- facevideo.py
+++ facevideo.py
@@ -2,6 +2,7 @@ import cv2
 
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -10,13 +11,18 @@ while True:
     
     _, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    
-    for (x, y, w, h) in faces:
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces: #for faces
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    
+        roi_gray = gray[y:y+h,x:x+w]# for eyes inside face
+        roi_color = img[y:y+h,x:x+w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes: #for eyes
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0,255 ,0), 2)
+
     cv2.imshow('img', img)
 
     # Stop if escape key is pressed
@@ -26,3 +32,4 @@ while True:
         
 
 cap.release()
+cv2.destroyAllWindows()
